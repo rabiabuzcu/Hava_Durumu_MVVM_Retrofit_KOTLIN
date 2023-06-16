c8712afce729b9c8ba9a0a256c19e8bc9886f950
rabiabuzcu <127779147+rabiabuzcu@users.noreply.github.com>
2023-06-16 14:57:12 +0300

    Hava_durumu_uygulaması
    hava durumu uygulaması

--- gradlew.bat
+++ gradlew.bat
@@ -1,11 +1,11 @@
 @if "%DEBUG%" == "" @echo off
-@rem ##########################################################################
 @rem
-@rem  Gradle startup script for Windows
 @rem
-@rem ##########################################################################
+@rem
+@rem
+@rem
 
-@rem Set local scope for the variables with windows NT shell
+@rem
 if "%OS%"=="Windows_NT" setlocal
 
 set DIRNAME=%~dp0
@@ -13,10 +13,10 @@ if "%DIRNAME%" == "" set DIRNAME=.
 set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%
 
-@rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
+@rem
 set DEFAULT_JVM_OPTS=
 
-@rem Find java.exe
+@rem
 if defined JAVA_HOME goto findJavaFromJavaHome
 
 set JAVA_EXE=java.exe
@@ -46,12 +46,12 @@ echo location of your Java installation.
 goto fail
 
 :init
-@rem Get command-line arguments, handling Windows variants
+@rem
 
 if not "%OS%" == "Windows_NT" goto win9xME_args
 
 :win9xME_args
-@rem Slurp the command line arguments.
+@rem
 set CMD_LINE_ARGS=
 set _SKIP=2
 
@@ -61,20 +61,20 @@ if "x%~1" == "x" goto execute
 set CMD_LINE_ARGS=%*
 
 :execute
-@rem Setup the command line
+@rem
 
 set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
 
-@rem Execute Gradle
+@rem
 "%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %CMD_LINE_ARGS%
 
 :end
-@rem End local scope for the variables with windows NT shell
+@rem
 if "%ERRORLEVEL%"=="0" goto mainEnd
 
 :fail
-rem Set variable GRADLE_EXIT_CONSOLE if you need the _script_ return code instead of
-rem the _cmd.exe /c_ return code!
+rem
+rem
 if  not "" == "%GRADLE_EXIT_CONSOLE%" exit 1
 exit /b 1
 
